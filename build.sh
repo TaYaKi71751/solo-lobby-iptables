32759b5e081ba6b25fc4b2b3f6b916c82b1bc661
TaYaKi71751 <shantian.tw@gmail.com>
2023-05-20 13:03:42 +0900

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+targets=("add-rules" "remove-rules" "enable-iptables" "disable-iptables")
+
+which shc || (\
+	echo "Install shc ( https://github.com/neurobin/shc )" && \
+	exit 1
+)
+
+for target in ${targets[@]};do
+	echo "shc -f ${target}.sh -o ${target}"
+	shc -f ${target}.sh -o ${target}.out
+done
